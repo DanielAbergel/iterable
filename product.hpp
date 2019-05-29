a45dc576935e6596d7d25e183da90a65cdd9e65c
DanielAbergel <danielabergel1@gmail.com>
2019-05-29 20:43:52 +0300

    finish product , test still working fine :)

--- product.hpp
+++ product.hpp
@@ -8,52 +8,58 @@ template <typename Templte_0, typename Templte_1>
 class product
 {
 private:
-    Templte_0 argument_0;
-    Templte_1 argument_1;
+    Templte_0 _begin;
+    Templte_1 _end;
+
+public:
+    product(Templte_0 arg1, Templte_1 arg2) : _begin(arg1), _end(arg2)
+    {
+    }
 
     template <typename it_0, typename it_1>
     class iterator
     {
 
     private:
-        it_0 it0;
-        it_1 it1;
-        bool Switch = true;
+        it_0 iterator_0;
+        it_1 iterator_1;
+        it_1 beginPtr;
 
     public:
-        iterator(it_0 first, it_1 second) : it0(first), it1(second)
+        iterator(it_0 first, it_1 second) : iterator_0(first), iterator_1(second), beginPtr(second)
         {
         }
 
-        std::pair<decltype(*it0), decltype(*it1)> operator*() const
+        std::pair<decltype(*iterator_0), decltype(*iterator_1)> operator*() const
         {
-            return std::pair<decltype(*it0), decltype(*it1)>(*it0, *it1);
+            return std::pair<decltype(*iterator_0), decltype(*iterator_1)>(*iterator_0, *iterator_1);
         }
+
         iterator<it_0, it_1> &operator++()
         {
+            ++iterator_1;
             return *this;
         }
-        bool operator==(const iterator<it_0, it_1> &other) const
+
+        bool operator!=(iterator<it_0, it_1> &other)
         {
-            return false;
-        }
-        bool operator!=(const iterator<it_0, it_1> &other)
-        {
-            return false;
+            if ((iterator_1 != other.iterator_1) == false)
+            {
+                ++iterator_0;
+                iterator_1 = beginPtr;
+            }
+
+            return iterator_0 != other.iterator_0 && iterator_1 != other.iterator_1;
         }
     };
-
-public:
-    product(Templte_0 arg1, Templte_1 arg2) : argument_0(arg1), argument_1(arg2)
+    auto begin() const
     {
+        return iterator<decltype(_begin.begin()), decltype(_end.begin())>(_begin.begin(), _end.begin());
     }
-    auto begin()
+    auto end() const
     {
-        return iterator<decltype(argument_0.begin()), decltype(argument_1.begin())>(argument_0.begin(), argument_1.begin());
-    }
-    auto end()
-    {
-        return iterator<decltype(argument_0.end()), decltype(argument_1.end())>(argument_0.end(), argument_1.end());
+        return iterator<decltype(_begin.end()), decltype(_end.end())>(_begin.end(), _end.end());
     }
 };
+
 } // namespace itertools
